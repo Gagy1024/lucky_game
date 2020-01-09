4f5771c2e36e6c2adeb7767b20d6a6e1c77c885b
rista.hcdo <rista.hcdo@gmail.com>
2020-01-09 21:21:58 +0100

    modified:   src/views/About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,13 +1,11 @@
 <template>
-  <div class="about" style="{'background-image':'url(../assets/background.png)'}">
-    <!--<Header />-->
-    <h1>INFO</h1>
-    <div id="levi"><img id="logo" src="../assets/logo-patkica.png"><p>LUCKY GAME</p></div>
-    <div id="desni">
-      <p>Nesto o aplikaciji....</p><br/>
-      <p>Pravila:<br/>...pravila??...</p>
-      <p id="tim">WebApp "Lucky Game" jer razvijena od strane Dragana Todorovica, Nenada Sarovica i Nikole Ristica</p>
-    </div>
+  <div class="about">
+    <Header />
+      <p>
+      Nesto o aplikaciji....<br/>
+      Pravila:<br/>...pravila??...<br/>
+      WebApp "Lucky Game" jer razvijena od strane Dragana Todorovica, Nenada Sarovica i Nikole Ristica
+      </p>
     <!--
       RECI AKO HOCES NESTO IZMENJENO!
       HVALA :D
@@ -16,44 +14,16 @@
 </template>
 
 <script>
-  //import Header from '../components/layout/Header.vue'
+  import Header from './../components/layout/Header.vue'
 
-  //export default {
-    //name: 'Header',
-    //components: {
-      //Header
-    //}
-  //0}
+  export default {
+    name: 'about',
+    components: {
+      Header
+     }
+   }
 </script>
 
 <style>
-  #tim
-    {
-      color: gray;
-    }
-  #levi
-    {
-      float: left;
-      border-radius: 30px;
-      background: white;
-      padding: 2%;
-      border: 3px solid green;
-    }
-  #logo
-    {
-      width: 17vw;
-      height: 17vw;
-      text-align: center;
-    }
-  #desni
-    {
-      text-align: center;
-      float: left;
-      border-radius: 30px;
-      background: white;
-      width: 60%;
-      padding: 5%;
-      margin-left: 7px;
-      border: 3px solid green;
-    }
+ 
 </style>
