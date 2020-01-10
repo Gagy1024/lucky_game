9bb87fdc53e1546188a27d4837fc1f7da5b75f88
rista.hcdo <rista.hcdo@gmail.com>
2020-01-10 19:20:32 +0100

    new file:   src/components/Counter.vue 	new file:   src/components/Gamelayout/Glkomb.vue

---
+++ src/components/Counter.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="counter">
+        <p>{{startTajmer}}</p>
+        <router-view />
+    </div>
+</template>
+
+<script>
+export default {
+    data ()
+        {
+            return{
+                startTajmer: 60
+            }
+        },
+    methods:
+        {
+            brojanje: function()
+                {
+                    if(this.startTajmer > 0)
+                        {
+                            setTimeout(()=>{
+                                this.startTajmer -= 1;
+                                this.brojanje();
+                            },1000)
+                        }
+                }
+        },
+    mounted: function()
+        {
+            this.brojanje();
+        }
+}
+</script>
---
+++ src/components/Gamelayout/Glkomb.vue
@@ -0,0 +1,81 @@
+<template>
+<div class="izvlacenje">
+    <p>{{broj}}</p><br>
+    <p>{{konacniniz[0]}}</p>
+    <p>{{konacniniz[1]}}</p>
+    <p>{{konacniniz[2]}}</p>
+    <p>{{konacniniz[3]}}</p>
+    <p>{{konacniniz[4]}}</p>
+    <p>{{konacniniz[5]}}</p>
+    <p>{{konacniniz[6]}}</p>
+    <p>{{konacniniz[7]}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            izvuceno: [],
+            tajmer: 5,
+            broj: "",
+            i: 0,
+            konacniniz: []
+        }
+    },
+methods:{
+    odborjavanje: function()
+        {
+            if(this.tajmer > 0 && this.i<this.izvuceno.length + 1 )
+                {
+                    setTimeout(()=>{
+                        this.tajmer -= 1;
+                        this.odborjavanje();
+                    },1000)
+                }
+            else
+                {
+                    this.broj = this.izvuceno[this.i];
+                    this.i += 1;
+                    if(this.i != this.izvuceno.length)
+                        {
+                            this.tajmer = 5;
+                          
+                            this.odborjavanje();
+                        }
+                    this.konacniniz.push(this.broj);
+
+                }
+            
+        },
+    brojevi: function()
+        {
+            var temp;
+            this.izvuceno = [];
+            for(let j=0;j<8;j++)
+                {
+                    temp = Math.floor(Math.random()*48)+1;
+                    if(!this.izvuceno.includes(temp))
+                        {
+                            this.izvuceno.push(temp);
+                        }
+                    else
+                        {
+                            --j;
+                        }
+                }
+        }
+},
+mounted: function()
+    {
+
+        this.brojevi();
+        this.odborjavanje();
+    }
+}
+
+</script>
+
+<style>
+
+</style>
