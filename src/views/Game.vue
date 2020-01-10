b33a049e96efd41d9a56ab2ba3f5cab4cb3c3ab1
rista.hcdo <rista.hcdo@gmail.com>
2020-01-10 21:41:31 +0100

    modified:   src/views/Game.vue

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="game">
     <Dodjela v-on:add-komb="addKomb" />
+    <counter v-on:load="Counter" />
   </div>
 </template>
 
 <script>
 import Dodjela from './../components/Gamelayout/Dodjela.vue';
+import Counter from './../components/Counter.vue';
 
 export default {
   name: 'Game',
   components:{
     Dodjela,
+    Counter,
   },
   data: function() {
     return {
