e6c42776c4300c7676f8281a516e722af4df68bd
Dragan Todorovic <dragantodorovic106@gmail.com>
2020-01-10 22:59:35 +0100

    modified:   src/views/Game.vue

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="game">
+    <Glkomb v-if="prekidac" />
     <Dodjela v-on:add-komb="addKomb" />
     <counter v-on:load="Counter" />
   </div>
@@ -8,17 +9,27 @@
 <script>
 import Dodjela from './../components/Gamelayout/Dodjela.vue';
 import Counter from './../components/Counter.vue';
+import Glkomb from './../components/Gamelayout/Glkomb.vue';
 
 export default {
   name: 'Game',
   components:{
     Dodjela,
     Counter,
+    Glkomb,
   },
   data: function() {
     return {
       korKombinacije: [],
+      prekidac: false,
     }
+  },
+  created() {
+    var self = this;
+    // Nakon 3 minute (180 sek) pokrenut će se funkcija getRezultat i promjeniti prekidac u false
+    setTimeout(function() {
+      self.prekidac = !self.prekidac;
+    }, 60000);
   },
   methods:{
     addKomb:function(newKomb){
