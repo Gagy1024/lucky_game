0cf4b285013800fb8503692b7b5f6501bcce201c
rista.hcdo <rista.hcdo@gmail.com>
2020-01-15 13:54:08 +0100

    modified:   src/components/Gamelayout/Provjera.vue

--- src/components/Gamelayout/Provjera.vue
+++ src/components/Gamelayout/Provjera.vue
@@ -2,17 +2,19 @@
   <div id="glavnakombinacija">
     <Header />
     <div id="komb">
-            <p>Kombinacija igre je:</p>
-            
-            <p>{{testni}}</p>
-            <hr>
+            <p id="tiketstat">Kombinacija igre je:</p>
+            <p id="tiketstat2"><b>{{testni[0]}}</b> , <b>{{testni[1]}}</b> , <b>{{testni[2]}}</b> , <b>{{testni[3]}}</b> , <b>{{testni[4]}}</b> , <b>{{testni[5]}}</b> , <b>{{testni[6]}}</b> , <b>{{testni[7]}}</b></p>
         </div>
       
       <div class="dobitak" v-bind:key="tiket" v-for="tiket in user">
-            <p>Id tiketa: {{tiket.idtiketa}}</p>
-            <p>Kombinacija: {{tiket.kombinacija}}</p>
-            <p>Broj pogodaka: {{tiket.broj}}</p>
-            <hr>
+            <p id="tiketstat">Id tiketa: <b>{{tiket.idtiketa}}</b></p>
+            <p id="tiketstat">Kombinacija: <b>{{tiket.kombinacija[0]}},
+                            {{tiket.kombinacija[1]}},
+                            {{tiket.kombinacija[2]}},
+                            {{tiket.kombinacija[3]}},
+                            {{tiket.kombinacija[4]}},
+                            {{tiket.kombinacija[5]}}</b></p>
+            <p id="tiketstat">Broj pogodaka: <b>{{tiket.broj}}</b></p>
         </div>
         </div>
 </template>
@@ -59,6 +61,33 @@ created:function(){
  
 </script>
 
-<style scoped>
-
+<style>
+.dobitak
+{
+  background-color: orange;
+  border-radius: 8px;
+  width: 300px;
+  height: 120px;
+  align-content: center;
+  margin-left: 38%;
+}
+#tiketstat
+{
+  color: black;
+  padding: 5px;
+}
+#komb
+{
+  background-color: rgba(255,165,0, 0.8);
+  border-radius: 8px;
+  width: 360px;
+  height: 90px;
+  align-content: center;
+  margin-left: 36%;
+}
+#tiketstat2
+{
+  color: black;
+  font-size: 22px;
+}
 </style>
